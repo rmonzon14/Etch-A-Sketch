d06a80c364ee49a0bed7c45ad547e7608359e364
Red Monzon <redkmonzon30@gmail.com>
2022-09-30 23:21:55 -0500

    Add a funtion to create cells

---
+++ script.js
@@ -0,0 +1,15 @@
+const container = document.getElementById('container');
+
+function createCells(cols, rows) {
+    for (i = 0; i < (cols * rows); i++) {
+        let cells = document.createElement('div');
+        container.appendChild(cells).classList.add('cells');
+    }
+    container.style.setProperty('--cols--repeat', cols);
+    container.style.setProperty('--rows--repeat', rows);
+}
+
+createCells(16, 16);
+
+
+
