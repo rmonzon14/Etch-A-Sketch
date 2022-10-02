14753e85d2c7599840da34e699fa1884a2f1468b
Red Monzon <redkmonzon30@gmail.com>
2022-10-01 19:16:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
-const container = document.getElementById('container');
-
+const grid = document.getElementById('grid');
 function createCells(cols, rows) {
     for (i = 0; i < (cols * rows); i++) {
-        let cells = document.createElement('div');
-        container.appendChild(cells).classList.add('cells');
+        let cell = document.createElement('div');
+        grid.appendChild(cell).classList.add('cells');
     }
-    container.style.setProperty('--cols-repeat', cols);
-    container.style.setProperty('--rows-repeat', rows);
+    grid.style.setProperty('--cols-repeat', cols);
+    grid.style.setProperty('--rows-repeat', rows);
 }
 
 createCells(16, 16);
 
-let cell = container.childNodes;
-
-cell.forEach(square => {
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'red';
+let cell = grid.childNodes;
+cell.forEach(squareDivs => {
+    squareDivs.addEventListener('mouseover', () => {
+        squareDivs.style.backgroundColor = 'red';
     })
+});
+
+const slider = document.getElementById('slider');
+const sliderValue = document.getElementById('current-slider-value');
+sliderValue.innerHTML = slider.value;
+sliderValue.innerHTML = slider.value + 'x' + slider.value;
+slider.oninput = function() {
+    sliderValue.innerHTML = this.value + 'x' + this.value;
+}
+
+const reset = document.getElementById('reset-button');
+reset.addEventListener('click', () => {
+    const squareCount = document.getElementsByClassName('cells');
+    for (i = 0; i < squareCount.length; i++) {
+        squareCount[i].style.backgroundColor = 'white';
+    }
 });
